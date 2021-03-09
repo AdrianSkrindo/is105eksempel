bf555dc5ab66e1ac086622d231ec82d18fd77f6d
Adrian Skrindo Lindland <adrianskrlindland@gmail.com>
2021-03-09 08:34:57 +0100

    Lagde to nye filer knyttet til HashSet modul 6, bare øvelse

---
+++ Hash.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Hash{
+
+public static void main(String[] args) {
+	HashSet<String> tall = new HashSet<String>();
+	tall.add("En");
+	tall.add("To");
+	tall.add("Tre");
+	for (String i : tall){
+	System.out.println(i);
+}
+}
+}
